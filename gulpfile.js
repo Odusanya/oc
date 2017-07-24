c304d2c98c5fe5026d64cd15ddbc8bd942bb4bba
Odusanya <odusanya.tomi@gmail.com>
2017-07-24 08:09:55 +0100

    style Get involved section

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var pngquant = require('imagemin-pngquant');
 ************* */
 
 gulp.task('imagemin', function () {
-    return gulp.src('./src/img/*')
+    return gulp.src('./img/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
